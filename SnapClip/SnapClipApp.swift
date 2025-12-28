b7f07a0591dbf559d81390de845955fd64116434
Jan Jędrzejak <jan.jedrzejak@gmail.com>
2025-12-28 21:32:31 +0100

    Initial Commit

---
+++ SnapClip/SnapClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapClipApp.swift
+//  SnapClip
+//
+//  Created by Jan Jedrzejak on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnapClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapClipUITests/SnapClipUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SnapClipUITestsLaunchTests.swift
+//  SnapClipUITests
+//
+//  Created by Jan Jedrzejak on 28/12/2025.
+//
+
+import XCTest
+
+final class SnapClipUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
